cd007771898ac40b94078832e07fe8a9cb99f362
Kazotaruu_ <kazotaruu@gmail.com>
2026-01-31 20:51:00 +0100

    Delete src/main/java/fr/robie/quantum directory

--- src/main/java/fr/robie/quantum/menu/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Menu system classes
- * 
- * - Menu: Represents a complete menu configuration
- * - MenuItem: Individual item in a menu
- * - MenuAction: Actions triggered on click
- * - Requirement: Conditions for viewing/clicking items
- */
-package fr.robie.quantum.menu;
--- src/main/java/fr/robie/quantum/utils/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Utility classes for Quantum plugin
- * 
- * - RequirementChecker: Validates player requirements
- * - ActionExecutor: Executes menu actions
- * - ItemBuilder: Builds ItemStacks with Nexo support
- * - Logger: Custom logging utility
- */
-package fr.robie.quantum.utils;

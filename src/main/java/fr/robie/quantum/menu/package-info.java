d7b1822628fd0aa94a95f60118e5bd7b7d117d2a
Kazotaruu_ <kazotaruu@gmail.com>
2026-01-31 04:24:18 +0100

    Add: .gitignore and final fixes before compilation

---
+++ src/main/java/fr/robie/quantum/menu/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Menu system classes
+ * 
+ * - Menu: Represents a complete menu configuration
+ * - MenuItem: Individual item in a menu
+ * - MenuAction: Actions triggered on click
+ * - Requirement: Conditions for viewing/clicking items
+ */
+package fr.robie.quantum.menu;
---
+++ src/main/java/fr/robie/quantum/utils/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Utility classes for Quantum plugin
+ * 
+ * - RequirementChecker: Validates player requirements
+ * - ActionExecutor: Executes menu actions
+ * - ItemBuilder: Builds ItemStacks with Nexo support
+ * - Logger: Custom logging utility
+ */
+package fr.robie.quantum.utils;

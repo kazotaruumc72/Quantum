6c667ca10d58ee1a2c962faaa4897bc876b98021
Kazotaruu_ <kazotaruu@gmail.com>
2026-02-01 17:29:15 +0100

    fix: Add missing STANDARD and QUANTUM_SELL button types

--- src/main/java/com/wynvers/quantum/menu/ButtonType.java
+++ src/main/java/com/wynvers/quantum/menu/ButtonType.java
@@ -4,9 +4,11 @@ package com.wynvers.quantum.menu;
  * Types de boutons spéciaux dans les menus
  */
 public enum ButtonType {
-    NONE,                    // Bouton normal
+    NONE,                    // Bouton normal (par défaut)
+    STANDARD,                // Bouton standard (alias de NONE)
     QUANTUM_CHANGE_MODE,     // Bouton pour changer le mode (vente/achat)
     QUANTUM_CHANGE_AMOUNT,   // Bouton pour changer la quantité
+    QUANTUM_SELL,            // Bouton de vente (ancien alias)
     QUANTUM_SELL_ITEM,       // Bouton pour vendre un item
     QUANTUM_ORDERS_ITEM      // Bouton dynamique pour afficher les ordres d'une catégorie
 }

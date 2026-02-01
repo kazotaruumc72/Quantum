fed8be8661dea4a91b05798d6dc74f5ef5dd3157
Kazotaruu_ <kazotaruu@gmail.com>
2026-02-01 01:32:07 +0100

    Create ButtonType enum with QUANTUM_CHANGE_MODE

---
+++ src/main/java/com/wynvers/quantum/menu/ButtonType.java
@@ -0,0 +1,18 @@
+package com.wynvers.quantum.menu;
+
+public enum ButtonType {
+    /**
+     * Bouton standard sans comportement spécial
+     */
+    STANDARD,
+    
+    /**
+     * Bouton pour changer le mode de storage (STORAGE <-> SELL)
+     */
+    QUANTUM_CHANGE_MODE,
+    
+    /**
+     * Slots de storage quantum (items stockés)
+     */
+    QUANTUM_STORAGE
+}
